49f8f482d4e3f8ec17e88d99f14874b8137b9bdb
Ryan Barton <ryan.d.barton@sce.com>
2025-10-22 07:21:27 -0700

    initial commit

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+// only intended to be used outside of hazel (applications)
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// ---ENTRY POINT----------------
+#include "Hazel/EntryPoint.h"
+// ------------------------------
---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,18 @@
+#include "Application.h"
+#include <iostream>
+namespace Hazel
+{
+
+	Application::Application()
+	{
+	}
+	Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		std::cout << "Hazel::Application::Run()" << std::endl;
+		while (true);
+	}
+}
